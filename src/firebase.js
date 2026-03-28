d7c489b4652951e6eae3b928c6a1a8032d74c235
Abhishek <user@users-MacBook-Pro.local>
2026-03-28 21:08:38 +0530

    Updated Firebase configuration with live credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Replace with your Firebase project configuration
 // You can find this in your Firebase Console: Project Settings -> General -> Your apps
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAtJMx2HbyAp1Z0XtLAZ-cNT-K6xyobR_I",
+  authDomain: "dayli-delivery.firebaseapp.com",
+  projectId: "dayli-delivery",
+  storageBucket: "dayli-delivery.firebasestorage.app",
+  messagingSenderId: "786855902799",
+  appId: "1:786855902799:web:3e7f23979ba7fd31df743a",
+  measurementId: "G-TC7FG9HSX6"
 };
 
 // Initialize Firebase
