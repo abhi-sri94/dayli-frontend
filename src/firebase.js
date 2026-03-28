465e50211ecad95c5c0f0852a5982d7719e63713
Abhishek <user@users-MacBook-Pro.local>
2026-03-28 22:24:31 +0530

    Fix: Corrected Firebase API key (j vs J mismatch)

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Replace with your Firebase project configuration
 // You can find this in your Firebase Console: Project Settings -> General -> Your apps
 const firebaseConfig = {
-  apiKey: "AIzaSyAtJMx2HbyAp1Z0XtLAZ-cNT-K6xyobR_I",
-  authDomain: "dayli-delivery.firebaseapp.com",
-  projectId: "dayli-delivery",
-  storageBucket: "dayli-delivery.firebasestorage.app",
+  apiKey: "AIzaSyATjMx2HbyAp1Z0XtLAZ-cNT-K6xyobR_I",
+  authDomain: "dalyi-delivery.firebaseapp.com",
+  projectId: "dalyi-delivery",
+  storageBucket: "dalyi-delivery.firebasestorage.app",
   messagingSenderId: "786855902799",
   appId: "1:786855902799:web:3e7f23979ba7fd31df743a",
   measurementId: "G-TC7FG9HSX6"
