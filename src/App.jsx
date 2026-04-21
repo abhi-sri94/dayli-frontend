39b6703e7ee3505ed026f97a4dce348a9b977d1c
Abhishek <user@users-MacBook-Pro.local>
2026-04-21 10:37:50 +0530

    feat: complete banner linking and category creation

--- src/App.jsx
+++ src/App.jsx
@@ -1828,9 +1828,9 @@ const PromoBanners = ({ banners, loading, onCategoryClick, onProductClick }) => 
 
 const SecondaryBanners = ({ onCategoryClick }) => {
   const banners = [
-    { title: 'Pharmacy', image: '/assets/banners/pharmacy_banner.png', id: null },
-    { title: 'Pet Care', image: '/assets/banners/pet_care_banner.png', id: null },
-    { title: 'Baby Care', image: '/assets/banners/baby_care_banner.png', id: null },
+    { title: 'Pharmacy', image: '/assets/banners/pharmacy_banner.png', id: 2 },
+    { title: 'Pet Care', image: '/assets/banners/pet_care_banner.png', id: 10 },
+    { title: 'Baby Care', image: '/assets/banners/baby_care_banner.png', id: 11 },
   ];
 
   return (
